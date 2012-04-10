b856dfc80f02de14ee60f874eb931e2d93af691b
Matt Chan <matt@mattchan.net>
2012-04-10 00:48:47 -0400

    fixed configuration

--- config/application.rb
+++ config/application.rb
@@ -57,5 +57,9 @@ module SwtTherapy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Don't connect to the database when precompiling (gets past Heroku issue)
+    config.assets.initialize_on_precompile = false
+    
   end
 end
