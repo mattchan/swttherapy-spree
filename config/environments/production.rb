1acc16a4590328c631e5ed517743d9a08d046243
Matt Chan <matt@mattchan.net>
2012-04-10 09:44:19 -0400

    recompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ SwtTherapy::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
@@ -44,7 +44,6 @@ SwtTherapy::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( store/all.js store/all.css admin/all.js admin/all.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
