f1f3f588c24477c45dc57a16efa4317c6855861c
Matt Chan <matt@mattchan.net>
2012-04-10 09:39:06 -0400

    updated assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ SwtTherapy::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( store/all.js store/all.css admin/all.js admin/all.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
